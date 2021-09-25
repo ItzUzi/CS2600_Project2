0bd83855fca45ee83bab615a670f7261b17c4974
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-25 00:44:31 -0700

    Started the airplane method will calculate time of arrival/departure

---
+++ AirplaneCosts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Method checks how long the trip was as well
+ * how long stay is
+ * Time related variables will be taken in 24 hr
+ * format
+ */
+void airPlane(){
+    double timeOfArrival;
+    
+}
