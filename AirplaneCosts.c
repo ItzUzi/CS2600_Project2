bd9d62848bbe2679ad20459bbceb255a66a0ec83
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-25 01:03:50 -0700

    Added days on trip function, returns amount of days user has stayed on trip

--- AirplaneCosts.c
+++ AirplaneCosts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * Method checks how long the trip was as well
@@ -11,6 +13,38 @@
 double airPlane(){
     double timeOfArrival;
 
+
+    return 0;
+}
+
+/**
+ * Asks user for amount of days they
+ * have stayed in trip
+ * Returns amount of days as type int
+*/
+int daysOnTrip(){
+    char input;
+    int days = 0;
+    printf("\nPlease input amount of days this trip will take.\n");
+    scanf("%s",&input);
     
+    days = atoi(&input);
+
+    if (days > 0 && atoi(&input))
+        return days;
+    else{
+        printf("\nSelect only numbers greater than 0\n");
+        return daysOnTrip();
+    }
+    
+}
+
+/**
+ * 
+*/
+int main(int argc, char *argv[]){
+    
+    daysOnTrip();
+
     return 0;
 }
