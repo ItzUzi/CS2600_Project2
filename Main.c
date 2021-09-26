f2fba7fa078b00c2849dd3ecf8fc432fe08aeedb
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-25 19:37:13 -0700

    Added call for departure in main method

--- Main.c
+++ Main.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include "DatesAndTime.h"
 
 /**
  * Program will run from here, this method will make calls
  * to other methods in order recieve all travel expenses
 */
 
+
+
 int main(int argc, char *argv[])
 {
+    double departure, arrival;
+    printf("Hello welcome to the Travel Expense calculator\n");
+    departure = setTime("Departure");
+
+
+
     return 0;
 }
