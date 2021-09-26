b380e18a3e7deea74f94edfad857645c4bebbb6e
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-26 01:05:33 -0700

    Added functions with documentation that will calculate vehicle costs

--- Main.c
+++ Main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include "DatesAndTime.h"
 #include "StayExpense.h"
+#include "TransportationExpenses.h"
 
 /**
  * Program will run from here, this method will make calls
  * to other methods in order recieve all travel expenses
 */
-
-
-
 int main(int argc, char *argv[])
 {
     int amountOfDays;
-    double departure, arrival;
+    double departure, arrival, transportCost;
     printf("Hello welcome to the Travel Expense calculator\n");
     amountOfDays = daysOnTrip();
     
@@ -30,7 +28,6 @@ int main(int argc, char *argv[])
 
     // creates array showing all meals, as well as differentiating between meals
     double meals[amountOfDays][3];
-
     mealExpenseDepartureToArrival(departure, arrival, meals, amountOfDays);
 
     for (int i = 0; i < amountOfDays; i++)
@@ -39,6 +36,8 @@ int main(int argc, char *argv[])
            printf("Meal cost is $%.2lf\n", meals[i][j]);
        }
     }
-    
+
+    transportCost = transportMethod();
+    printf("Total transport cost: %.2lf", transportCost);
     return 0;
 }
