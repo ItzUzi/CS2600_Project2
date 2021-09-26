b6e46bd19e52287e3b805d8d3a9f5e0cb3b8acc2
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-25 18:47:16 -0700

    Started to implement the Main file where all method calls will be done

---
+++ Main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * Program will run from here, this method will make calls
+ * to other methods in order recieve all travel expenses
+*/
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
