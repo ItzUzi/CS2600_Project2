2558941827bc93d559cdb040aca86963ad5f947f
Uziel Gaeta <ugaeta@cpp.edu>
2021-09-25 23:36:17 -0700

    Made new file in which transportation expenses will take place, airfare, vehicle

---
+++ TransportationExpenses.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Method checks how long the trip was as well
+ * how long stay is
+ * Time related variables will be taken in 24 hr
+ * format
+ * 
+ * Returns costs of airfare
+ */
+double airPlane(){
+    double timeOfArrival;
+
+
+    return 0;
+}
